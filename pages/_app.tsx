7b1e446bb9757aa88db58d663e936da9c324235b
Eirik Dahlen <dahleneirik@gmail.com>
2023-01-04 09:35:36 +0100

    Move decorator to components folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Decorator from "./decorator/Decorator";
+import Decorator from "../components/decorator/Decorator";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div>
