e096dc406e79f6af9aa3023ab13ba4ea320725df
Eirik Dahlen <dahleneirik@gmail.com>
2023-01-09 12:39:19 +0100

    Add hydration for react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,38 @@
+import React, {useState} from "react";
 import '../styles/globals.css'
 import "@navikt/ds-css";
 import type {AppProps} from 'next/app'
 import Decorator from "../components/decorator/Decorator";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-
-const queryClient = new QueryClient(
-    {
-        defaultOptions: {
-            queries: {
-                refetchOnWindowFocus: false,
-                /*cacheTime: minutesToMillis(60),
-                staleTime: minutesToMillis(30),
-                retry: (failureCount, error) => {
-                  if (isClientError(error)) {
-                    return false;
-                  }
-
-                  return failureCount < 3;
-                },*/
-            },
-        },
-    }
-);
+import {Hydrate, QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {minutesToMillis} from "../utils/timeUtils";
+import {isClientError} from "../api/errors";
 
 export default function App({Component, pageProps}: AppProps) {
+    const [queryClient] = useState(() => new QueryClient({
+            defaultOptions: {
+                queries: {
+                    refetchOnWindowFocus: false,
+                    cacheTime: minutesToMillis(60),
+                    staleTime: minutesToMillis(30),
+                    retry: (failureCount, error) => {
+                      if (isClientError(error)) {
+                        return false;
+                      }
+
+                      return failureCount < 3;
+                    },
+                },
+            },
+        }
+    ))
+
     return (
         <>
             <Decorator/>
             <QueryClientProvider client={queryClient}>
-                <Component {...pageProps} />
+                <Hydrate state={pageProps.dehydratedState}>
+                    <Component {...pageProps} />
+                </Hydrate>
             </QueryClientProvider>
         </>
     )
